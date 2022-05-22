8fb76734de4f775d3d1e9a3faa6784f042370f2a
gdwb3219 <gdwb3219@gmail.com>
2022-05-22 22:16:55 +0900

    Add React Adfit Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
-import Board from './Board'
+import Board from './Board';
 import Button from './Button';
-// import './App.css';
+import logo from './assets/logo.png';
+import React from 'react';
+import IntegratedAdfitComponent from './IntegratedAdfitComponent';
 
 function random(n) {
   return Math.ceil(Math.random() * n);
@@ -10,11 +12,11 @@ function random(n) {
 function App() {
   const [num, setNum] = useState(1);
   const [sum, setSum] = useState(0);
-  const [gameHistory, setGameHistory] = useState([0]);  // 배열 (참조형이기 때문에)
+  const [gameHistory, setGameHistory] = useState([0]); // 배열 (참조형이기 때문에)
   const [otherNum, setOtherNum] = useState(1);
   const [otherSum, setOtherSum] = useState(0);
-  const [otherGameHistory, setOtherGameHistory] = useState([0]);  // 배열 (참조형이기 때문에)
-  
+  const [otherGameHistory, setOtherGameHistory] = useState([0]); // 배열 (참조형이기 때문에)
+
   const handleRollClick = () => {
     const nextNum = random(6);
     setNum(nextNum);
@@ -38,14 +40,42 @@ function App() {
   return (
     <div className="App">
       <div>
-        <Button className="App-button" color="blue" onClick={handleRollClick}>던지기</Button>
-        <Button className="App-button" color="red" onClick={handleClearClick}>처음부터</Button>
+        <img class="App-logo" src={logo} alt="주사위게임 로고" />
+        <h1 class="App-title">주사위게임</h1>
       </div>
-      <div className='buttonItem'>
-        <Board name = "나" color="blue" num={num} sum={sum} gameHistory={gameHistory} /><Board name = "상대" color="red" num={otherNum} sum={otherSum} gameHistory={otherGameHistory} />
+      <div>
+        <Button className="App-button" color="blue" onClick={handleRollClick}>
+          던지기
+        </Button>
+        <Button className="App-button" color="red" onClick={handleClearClick}>
+          처음부터
+        </Button>
+      </div>
+      <div class="App-boards">
+        <div class="Board App-board">
+          <Board
+            name="나"
+            color="blue"
+            num={num}
+            sum={sum}
+            gameHistory={gameHistory}
+          />
+        </div>
+        <div class="Board App-board">
+          <Board
+            name="상대"
+            color="red"
+            num={otherNum}
+            sum={otherSum}
+            gameHistory={otherGameHistory}
+          />
+        </div>
+      </div>
+      <div class="ad-banner">
+        <IntegratedAdfitComponent />
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ src/IntegratedAdfitComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AdfitWebComponent from 'react-adfit-web-component';
+
+class IntegratedAdfitComponent extends React.Component {
+  render() {
+    return <AdfitWebComponent adUnit="DAN-1oCLX9vl0SSsmZgg" />;
+  }
+}
+
+export default IntegratedAdfitComponent;
